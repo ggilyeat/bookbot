61e25efbeae0766debd2645686957e3dbc94fa2f
ggilyeat <jgilyeat@gmail.com>
2025-09-15 17:23:49 -0400

    final update for bookbot initial lesson

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/sbin/python3
 
+import sys
 from stats import count_words, get_char_dict, sort_dict
 
 def get_book_text( filepath ):
@@ -7,17 +8,22 @@ def get_book_text( filepath ):
         return f.read()
 
 def main():
-    path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    path = sys.argv[1]
     text = get_book_text(path)
     char_list = sort_dict(get_char_dict(text))
 
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {path}...")
     print("----------- Word Count ----------")
     print(f"Found {count_words(text)} total words")
     print("--------- Character Count -------")
     for item in char_list:
         print(f"{item["char"]}: {item["num"]}")
+    print("---------------------------------")
 
 if __name__ == "__main__":
     main()
