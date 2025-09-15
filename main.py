325761008124e044b1892e6147cc9d4045de22be
ggilyeat <jgilyeat@gmail.com>
2025-09-15 17:18:09 -0400

    Sorted Dict lesson

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-from stats import count_words
-from stats import count_chars
+#!/usr/sbin/python3
+
+from stats import count_words, get_char_dict, sort_dict
+
+def get_book_text( filepath ):
+    with open(filepath, encoding='utf-8-sig') as f:
+        return f.read()
 
 def main():
     path = "books/frankenstein.txt"
-    print( f"{count_words(path)} words found in the document" )
-    print( f"{count_chars(path)} characters found in the document.")
+    text = get_book_text(path)
+    char_list = sort_dict(get_char_dict(text))
 
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {count_words(text)} total words")
+    print("--------- Character Count -------")
+    for item in char_list:
+        print(f"{item["char"]}: {item["num"]}")
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -1,6 +1,24 @@
-def get_book_text( filepath ):
-    with open(filepath) as f:
-        return f.read()
+def count_words(text):
+    return len(text.split())
 
-def count_words(filepath):
-    return len(get_book_text(filepath).split())
+def get_char_dict(text):
+    retval = {}
+    for ch in list(text):
+        ch = ch.lower()
+        if ch in retval:
+            retval[ch] += 1
+        else: 
+            retval[ch] = 1
+    return retval
+
+def sort_on(items):
+    return items["num"]
+
+def sort_dict(d):
+    retval = list()
+    for k in d:
+        retval.append({"char": k, "num": d[k]})
+    
+    retval.sort(reverse=True, key=sort_on)
+
+    return retval
